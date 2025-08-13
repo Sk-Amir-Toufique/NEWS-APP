cbe44a0a03c946918df67b84a388150c8d8adc69
Sk Amir Toufique <toufiquesk13@gmail.com>
2025-08-13 12:54:14 +0530

    done

--- news-app/src/App.jsx
+++ news-app/src/App.jsx
@@ -17,7 +17,13 @@ export default class App extends Component {
         <Router>
           <Navbar/>
           <Routes>
-            <Route path="/" element={<News pageSize={6} country="us" category="science"/>} />
+            <Route path="/" element={<News pageSize={6} country="us" category="general"/>} />
+            <Route path="/business" element={<News pageSize={6} country="us" category="business"/>} />
+            <Route path="/entertainment" element={<News pageSize={6} country="us" category="entertainment"/>} />
+            <Route path="/health" element={<News pageSize={6} country="us" category="health"/>} />
+            <Route path="/science" element={<News pageSize={6} country="us" category="science"/>} />
+            <Route path="/sports" element={<News pageSize={6} country="us" category="sports"/>} />
+            <Route path="/technology" element={<News pageSize={6} country="us" category="technology"/>} />
           </Routes>
         </Router>
       </div>
